041b4ab814e66d2a2847e32ea8c95c7ac74c7113
Nihal Srivastava <82692408+Nihal-Srivastava05@users.noreply.github.com>
2022-10-28 10:59:57 +0530

    Additional Features added for URL Classification.

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -3,6 +3,9 @@ from datetime import datetime, timezone
 import math
 import warnings
 
+from re import compile
+from urllib.parse import urlparse
+from socket import gethostbyname
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -10,9 +13,10 @@ class UrlFeaturizer(object):
     def __init__(self, url):
         self.url = url
         self.domain = url.split('//')[-1].split('/')[0]
+        self.urlparse = urlparse(url)
 
     def entropy(self):
-        string = self.url.strip()
+        string = self.url.lower().strip()
         prob = [float(string.count(c)) / len(string) for c in dict.fromkeys(list(string))]
         entropy = sum([(p * math.log(p) / math.log(2.0)) for p in prob])
         return entropy
@@ -57,15 +61,101 @@ class UrlFeaturizer(object):
         ext = self.url.split('.')[-1].split('/')[0]
         return ext
 
-    ## URL domain features
     def hasHttp(self):
         return 'http:' in self.url
 
     def hasHttps(self):
         return 'https:' in self.url
 
+    def url_host_is_ip(self):
+        host = self.urlparse.netloc
+        pattern = compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+        match = pattern.match(host)
+        return match is not None
+
+    def get_ip(self):
+        try:
+            ip = self.urlparse.netloc if self.url_host_is_ip() else gethostbyname(self.urlparse.netloc)
+            return ip
+        except:
+            return None
+
+    def url_path_length(self):
+        return len(self.urlparse.path)
+
+    def url_host_length(self):
+        return len(self.urlparse.netloc)
+
+    def url_has_port_in_string(self):
+        has_port = self.urlparse.netloc.split(':')
+        return len(has_port) > 1 and has_port[-1].isdigit()
+
+    def is_encoded(self):
+        return '%' in self.url.lower()
+
+    def num_encoded_char(self):
+        encs = [i for i in self.url if i == '%']
+        return len(encs)
+
+    def number_of_subdirectories(self):
+        d = self.urlparse.path.split('/')
+        return len(d)
+
+    def number_of_periods(self):
+        periods = [i for i in self.url if i == '.']
+        return len(periods)
+
+    def has_client_in_string(self):
+        return 'client' in self.url.lower()
+
+    def has_admin_in_string(self):
+        return 'admin' in self.url.lower()
+
+    def has_server_in_string(self):
+        return 'server' in self.url.lower()
+
+    def has_login_in_string(self):
+        return 'login' in self.url.lower()
+        
+    def get_tld(self):
+        #top-level domain
+        return self.urlparse.netloc.split('.')[-1].split(':')[0]
+
+    def count_arate(self):
+        arates = [i for i in self.url if i == '@']
+        return len(arates)
+
+    def count_asterisk(self):
+        asterisks = [i for i in self.url if i == '*']
+        return len(asterisks)
+
+    def count_questionmark(self):
+        questionmarks = [i for i in self.url if i == '?']
+        return len(questionmarks)
+
+    def count_plus(self):
+        plus = [i for i in self.url if i == '+']
+        return len(plus)
+
+    def count_exclamation(self):
+        exclamation = [i for i in self.url if i == '!']
+        return len(exclamation)
+
+    def count_hyphen(self):
+        hyphen = [i for i in self.url if i == '-']
+        return len(hyphen)
+
+    def count_equal(self):
+        equals = [i for i in self.url if i == '=']
+        return len(equals)
+
+    def count_tilted(self):
+        tilted = [i for i in self.url if i == '~']
+        return len(tilted)
+
     def run(self):
         data = {}
+        data['url'] = self.url
         data['entropy'] = self.entropy()
         data['numDigits'] = self.numDigits()
         data['urlLength'] = self.urlLength()
@@ -76,6 +166,26 @@ class UrlFeaturizer(object):
         data['num_%20'] = self.url.count("%20")
         data['num_@'] = self.url.count("@")
         data['has_ip'] = self.ip()
+        data['path_length'] = self.url_path_length()
+        data['host_length'] = self.url_host_length()
+        data['has_port'] = self.url_has_port_in_string()
+        data['is_encoded'] = self.is_encoded()
+        data['num_encoded_char'] = self.num_encoded_char()
+        data['number_of_subdirectories'] = self.number_of_subdirectories()
+        data['number_of_periods'] = self.number_of_periods()
+        data['has_client_in_string'] = self.has_client_in_string()
+        data['has_admin_in_string'] = self.has_admin_in_string()
+        data['has_server_in_string'] = self.has_server_in_string()
+        data['has_login_in_string'] = self.has_login_in_string()
+        data['tld'] = self.get_tld()
+        data['count_arate'] = self.count_arate()
+        data['count_asterisk'] = self.count_asterisk()
+        data['count_questionmark'] = self.count_questionmark()
+        data['count_plus'] = self.count_plus()
+        data['count_exclamation'] = self.count_exclamation()
+        data['count_hyphen'] = self.count_hyphen()
+        data['count_equal'] = self.count_equal()
+        data['count_tilted'] = self.count_tilted()
     
         return data
 
@@ -97,13 +207,12 @@ def feature_extraction(dataset, FEATURE_DFS):
     FEATURE_DFS.append(feature_df)
     print("DONE {}".format(os.getpid()))
 
-
 if __name__ == "__main__":
 	with Manager() as manager:
 		FEATURE_DFS = manager.list()
 		processes = []
-		idx = 10000
-		start_idx = 630002
+		idx = 15000
+		start_idx = 650001
 		for i in range(1, 15):
 			p = Process(target=feature_extraction, args=(df[start_idx+idx*(i-1):start_idx+idx*i], FEATURE_DFS))
 			p.start()
--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Make a Web Application Firewall (WAF) using python to block harmful websites, bl
 proxy --log-file log_file.txt
 
 - Set that firewall as your proxy in the firefox (Use Foxy proxy extension)
+
+
+### References and Articles:
+- https://towardsdatascience.com/extracting-feature-vectors-from-url-strings-for-malicious-url-detection-cbafc24737a  
+- https://medium.com/nerd-for-tech/url-feature-engineering-and-classification-66c0512fb34d
+- https://github.com/lucasayres/url-feature-extractor
